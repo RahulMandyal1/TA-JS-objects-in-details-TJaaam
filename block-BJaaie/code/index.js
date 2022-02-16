46a0ef86f0311966d82bf3950ffdcee509fcd46a
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-02-16 12:11:32 +0530

    Updated Assignment

--- block-BJaaie/code/index.js
+++ block-BJaaie/code/index.js
@@ -78,3 +78,82 @@ Cat.prototype = {
 Object.setPrototypeOf(Cat.prototype , Animal.prototype);
 
 let kitty = new Cat("kitty","brownRed");
+
+
+
+// Now Classical Pattern 
+
+class Animal{
+    constructor(location , numberOfLegs){
+        this.location = location ;
+        this.numberOfLegs = numberOfLegs;
+    }
+    eat(){
+        console.log(`I live in ${this.location} and I can eat`);
+    }
+    changeLocation(newLocation){
+        this.location = newLocation;
+    }
+    summary(){
+        return ` I live in ${this.location} and I have ${this.numberOfLegs} legs`;
+    }
+
+}
+
+
+let animal  =  new Animal("Dharmshal",4);
+console.dir(animal);
+console.log(animal.eat());
+console.log(animal.changeLocation('jungle'));
+console.log(animal.summary());
+
+
+class Dog extends Animal{
+//     Animal.apply(this,["Alt-campus",4]);
+       constructor( location , numberOfLegs,name , color){
+        this.name = name;
+        this.color = color;
+        super(location,numberOfLegs);
+
+       }
+    
+       bark(){
+          alert(`I am ${ this.name} and I can bark 🐶`);
+       }
+       changeName(newName){
+          this.name = newName;
+        }
+       changeColor(newColor){
+          this.color =newColor;
+       }
+        summary(){
+           return `I am ${this.name} and I am of ${this.color} color. I can also bark`;
+        }
+}
+
+let pochi = new Animal("alt-campus","4","Pochi ", "BrownYellow");
+class Cat extends Animal{
+    //     Animal.apply(this,["Alt-campus",4]);
+           constructor(location , numberOfLegs,name , eyeColor){
+            super(location,numberOfLegs);
+             this.name = name;
+             this.colorOfEyes = eyeColor;
+           }
+           
+           meow(){
+                 alert(`I am ${this.name} and I can do mewo meow 😹`);
+            }
+            changeName(newName){
+                 this.name = newName;
+            }
+            changeColorOfEyes(newColor){
+                   this.colorOfEyes =newColor;
+            }
+            summary(){
+                 return `I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can also do meow meow`
+            }
+          
+}
+
+
+let kitty = new Cat("alt-campus","4","kitty","red");
